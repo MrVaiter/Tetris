9f2e8edec8c1cc57045d5debbf05d84c3db940c3
Dmytro Molchanov <dima-25@ukr.net>
2022-11-25 01:52:09 +0200

    Add random select for tetraminoes

--- app.js
+++ app.js
@@ -45,10 +45,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
 
+    // Start position
     let currentPosition = 4;
-    let current = theTetrominoes[0][0];
+    let currentRotation = 0;
 
-    function draw(){
+    // Random select
+    let random = Math.floor(Math.random() * theTetrominoes.length);
+    let current = theTetrominoes[random][currentRotation];
+
+    // Draw the tetramino
+    function draw() {
         current.forEach(index => {
             squares[currentPosition + index].classList.add('tetromino');
         });
