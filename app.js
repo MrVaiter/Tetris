590735d952841e90abd35df89ff31a4a253512e7
Dmytro Molchanov <dima-25@ukr.net>
2022-11-25 17:04:01 +0200

    Add freeze function

--- app.js
+++ app.js
@@ -76,6 +76,20 @@ document.addEventListener('DOMContentLoaded', () => {
         undraw();
         currentPosition += width;
         draw();
+        freeze();
+    }
+
+    // Freeze function
+    function freeze(){
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'));
+
+            // Start new tetromino fall
+            random = Math.floor(Math.random() * theTetrominoes.length);
+            current = theTetrominoes[random][currentRotation];
+            currentPosition = 4;
+            draw();
+        }
     }
 
     draw();
